bef7922350a16153cad4acddc3fa6aa5731586f7
vid553 <vid.rajtmajer@gmail.com>
2018-08-24 13:28:10 +0200

    Update raspbian_install.sh

--- scripts/raspbian_install.sh
+++ scripts/raspbian_install.sh
@@ -45,7 +45,7 @@ sudo mkdir /data/
 
 # manual execute
 echo "---------------------------------------------------"
-echo "Execute raspi-config and enable I2C,SPI and Serial"
+echo "Execute raspi-config and enable I2C, SPI and Serial (when asked to enable shell: NO, hardware: YES)"
 echo "---------------------------------------------------"
 echo "Edit /boot/config and paste the following:         "
 echo "dtoverlay=pi3-miniuart-bt"
