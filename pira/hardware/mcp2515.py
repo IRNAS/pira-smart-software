483fc6e1adf5fd60052c79708f93b43d4ba52d0b
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2018-09-10 13:12:13 +0200

    Fixed mcp timeout

--- pira/hardware/mcp2515.py
+++ pira/hardware/mcp2515.py
@@ -49,24 +49,14 @@ class MCP2515():
         return self._enabled
     
     def get_raw_data(self):
-        self._message = self._bus.recv()
+        self._message = self._bus.recv(3.0)
         if self._message is not None:
             return self._message
         return None
 
     def get_data(self):
-        """ waits until nothing received 
-        timeouts after 3 seconds
-        """
-        timeout = 0
-        while timeout < 3:
-            try:
-                self._message = self._bus.recv()
-                break
-            except:
-                timeout += 1
-                time.sleep(1)
-            
+        """ waits until nothing received, timeouts after 3 seconds """
+        self._message = self._bus.recv(3.0)
         if self._message is not None:
             c = '{0:f} {1:x} {2:x} '.format(self._message.timestamp, self._message.arbitration_id, self._message.dlc)
             s = ''
