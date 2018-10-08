60f1808aea4fffe83a34601664dcd9e97bc541ad
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2018-10-08 18:42:59 +0200

    test

--- pira/boot.py
+++ pira/boot.py
@@ -364,7 +364,7 @@ class Boot(object):
     def _perform_shutdown(self):
         """Perform shutdown."""
 
-        # check if device is ready to shutdown (status of device is Idle)
+        # check if device is ready to shutdown: status of device is Idle
         device_status = self._resin.models.supervisor.get_device_state()
         if device_status['status'] != 'Idle':
             print ("Device not ready to shutdown, its status is : " + str(device_status['status']))
