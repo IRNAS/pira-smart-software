cc0c13e2bb1ff28d71cb38dffb999c8d52e1dc76
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2019-02-19 10:50:14 +0100

    csv version handle

--- pira/modules/processing.py
+++ pira/modules/processing.py
@@ -65,7 +65,10 @@ class Module(object):
         with open(config_full_path, "rb") as fp:
             self._config_file = json.load(fp)
         #print(self._config_file)
-        config_file_version = str(self._config_file['version'])
+        try:
+            config_file_version = str(self._config_file['version'])
+        except:
+            config_file_version = 1
         self._csv_filename = CSV_DATA_STORAGE_PATH  + '/' + filename + "-v" + config_file_version + ".csv"
 
         # prepare dictionaries of raw and calculated data
