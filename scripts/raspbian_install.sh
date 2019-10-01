b405b7aefa21f90ae82f3dee0a1dbb5c0c346f15
Tjaž Vračko <tjaz.vracko@gmail.com>
2019-10-01 14:40:27 +0200

    Disabled all service disables

--- scripts/raspbian_install.sh
+++ scripts/raspbian_install.sh
@@ -59,13 +59,13 @@ sudo systemctl reenable pira.service
 echo "Disabling unnecessary services"
 # sudo systemctl disable dhcpcd.service
 # sudo systemctl disable networking.service
-sudo systemctl disable keyboard-setup.service
+# sudo systemctl disable keyboard-setup.service
 # sudo systemctl disable avahi-daemon.service
 # sudo systemctl disable ssh.service
-sudo systemctl disable bluetooth.service
+# sudo systemctl disable bluetooth.service
 # sudo systemctl disable hostapd.service
 # sudo systemctl disable udhcpd.service
-sudo systemctl disable hciuart
+# sudo systemctl disable hciuart
 
 sudo systemctl --system daemon-reload
 
