ace167d1d0367fc8503fa74c009a175e15516b59
Tjaž Vračko <tjaz.vracko@gmail.com>
2019-10-11 15:31:39 +0200

    Added info prints to script

--- scripts/read-only-setup.sh
+++ scripts/read-only-setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # WARNING: did not test what happens if this script is run twice
-
+echo "Removing triggerhappy logrotate dphys-swapfile"
 sudo apt-get remove -y --purge triggerhappy logrotate dphys-swapfile
 
 sudo apt-get autoremove --purge -y
-
+echo "Editing /boot/cmdline.txt"
 sudo sed -i -r "$ s/(.*)/\1 fastboot noswap ro/" /boot/cmdline.txt
 
+echo "Installing log manager"
 sudo apt-get install -y busybox-syslogd
 sudo apt-get remove -y --purge rsyslog
 
 # fstab edit
+echo "Editing /etc/fstab"
 sudo cp /etc/fstab /etc/fstab.bak
 
 awk '$2=="/boot"{$4=$4",ro"}1' /etc/fstab > tmp && sudo mv tmp /etc/fstab
@@ -22,6 +24,7 @@ echo "tmpfs        /var/log        tmpfs   nosuid,nodev         0       0" | sud
 echo "tmpfs        /var/tmp        tmpfs   nosuid,nodev         0       0" | sudo tee -a /etc/fstab > /dev/null
 
 # simlink setup
+echo "Setting simlinks"
 sudo rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/spool /etc/resolv.conf
 sudo ln -s /tmp /var/lib/dhcp
 sudo ln -s /tmp /var/lib/dhcpcd5
@@ -30,12 +33,14 @@ sudo touch /tmp/dhcpcd.resolv.conf
 sudo ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
 
 # update random seed
+echo "Updating random seed setup"
 sudo rm /var/lib/systemd/random-seed
 sudo ln -s /tmp/random-seed /var/lib/systemd/random-seed
 
 echo "ExecStartPre=/bin/echo \"\" >/tmp/random-seed" | sudo tee -a /lib/systemd/system/systemd-random-seed.service > /dev/null
 
 # add ro and wr commands to bashrc
+echo "Adding ro/rw commands to bashrc"
 
 echo "
 set_bash_prompt() {
@@ -53,5 +58,7 @@ sudo touch /etc/bash.bash_logout
 echo "mount -o remount,ro /
 mount -o remount,ro /boot" | sudo tee -a /etc/bash.bash_logout > /dev/null
 
+
+echo "Done"
 # TODO:
 # https://medium.com/swlh/make-your-raspberry-pi-file-system-read-only-raspbian-buster-c558694de79
